8326434af5b93d0a04f3cf91b9b59297befc09b9
dannyid <danielshekhtman@gmail.com>
2015-04-22 00:39:10 -0400

    Fixing weird tabbing issues.

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ $(document).keydown(function(event) {
 function init() {
   var $dot = $("#dot"),
       distance = 10,
-		  rate = 40,
-		  min = 0,
-		  maxY,
-		  maxX;
+      rate = 40,
+      min = 0,
+      maxY,
+      maxX;
 
   var controllerListenerInterval = setInterval(controllerListener, 5);
   
@@ -19,7 +19,7 @@ function init() {
     var direction = "";
     var angle = 0;
 
-	  var moveInterval = setInterval(move, rate);
+    var moveInterval = setInterval(move, rate);
     
     function move() {
       var left = parseInt($dot.css('left'));
@@ -28,19 +28,19 @@ function init() {
       if (direction === "L" && left > min) { // left
         $dot.css("left", left - distance+"px");
         rotate(-30);
-      } 
+      }
       else if (direction === "R" && left < maxX) { // right
         $dot.css("left", left + distance+"px");
         rotate(30);
-      } 
+      }
       else if (direction === "U" && top > min) { // up
         $dot.css("top", top - distance+"px");
-      } 
+      }
       else if (direction === "D" && top < maxY) { // down
         $dot.css("top", top + distance+"px");
       }
     }
-
+    
     function rotate(deg) {
       angle = (angle += deg) % 360;
       $dot.css('transform', 'rotateZ('+angle+'deg)');
@@ -49,18 +49,18 @@ function init() {
     function changeDir(k) {
       if (k === 37 || k.gpLeft === true) { // left
         direction = "L";
-      } 
+      }
       else if (k === 39 || k.gpRight === true) { // right
         direction = "R";
-      } 
+      }
       else if (k === 38 || k.gpUp === true) { // up
         direction = "U";
-      } 
+      }
       else if (k === 40 || k.gpDown === true) { // down
         direction = "D";
       } else if (k === 32) { // stop
         direction = ""; 
-      } 
+      }
     }
 
     return {
@@ -79,7 +79,7 @@ function init() {
 	// 		gpRight: gp[15].pressed
 	// 	};
 
-	// 	game.dot.changeDir(buttonPressed);
+  // 	game.dot.changeDir(buttonPressed);
 	}
 
 	(function calcMax() {
